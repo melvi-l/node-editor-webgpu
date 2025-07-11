e1156ad5d72beb8357455b4003a111336eb57025
melvi-l <melvil.miglio@gmail.com>
2025-07-12 00:59:26 +0200

    chore: change base url

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-    base: "/wgpu-node-editor/",
+    base: "/node-editor-webgpu/",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "src"),
